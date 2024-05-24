9245e1e77430271fb97a8fc3fd79ca373165a64b
giosaginashvili <164656337+giosaginashvili@users.noreply.github.com>
2024-05-24 10:33:26 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let myage = 14
+let myname = "giorgi"
+let myhobbi= "play rugby"
+let yearofbirth = 2010
+console.log("my name is"+myname )
+console.log("my age is "+myage )
+console.log("my hobbi is"+myhobbi )
+console.log("yearofbirth is"+yearofbirth )
